af3fa3aac4ee88a104f7f536df7214f31c9a56c6
RuinedVLR <rzhurakhov@gmail.com>
2025-10-25 00:04:28 -0300

    ColorMap for generator

--- Assets/Scripts/PerlinMapGenerator/Noise.cs
+++ Assets/Scripts/PerlinMapGenerator/Noise.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octavesOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            octavesOffsets[i] = new Vector2(offsetX, offsetY);
+
+        }
 
         if (scale <= 0)
         {
@@ -15,6 +25,9 @@ public static class Noise
 
         float maxNoiseHeight = float.MinValue;
         float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2;
+        float halfHeight = mapHeight / 2;
 
         for (int y = 0; y < mapHeight; y++)
         {
@@ -26,8 +39,8 @@ public static class Noise
                 
                 for (int i = 0; i < octaves; i++)
                 {
-                    float sampleX = x / scale * frequency;
-                    float sampleY = y / scale * frequency;
+                    float sampleX = (x - halfWidth) / scale * frequency + octavesOffsets[i].x;
+                    float sampleY = (y - halfHeight) / scale * frequency + octavesOffsets[i].y;
 
                     float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
                     noiseHeight += perlinValue * amplitude;
