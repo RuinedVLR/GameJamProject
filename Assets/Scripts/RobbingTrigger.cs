42be69a332ef8fff2c0a472a4332c0bb1159394e
RuinedVLR <rzhurakhov@gmail.com>
2025-10-26 12:16:01 -0300

    Money and robbing

--- Assets/Scripts/RobbingTrigger.cs
+++ Assets/Scripts/RobbingTrigger.cs
@@ -8,14 +8,14 @@ public class RobbingTrigger : MonoBehaviour
 {
     public float robTimer = 3f;
     public float stayTimer = 0;
-    //public TextMeshProUGUI moneyCount;
+    public TextMeshProUGUI moneyCount;
     public int money = 0;
     private System.Random rand = new();
 
     // Update is called once per frame
     void Update()
     {
-        //moneyCount.text = "Money: " + money.ToString();
+        moneyCount.text = "Money: $" + money.ToString();
     }
 
     private void OnTriggerStay(Collider other)
