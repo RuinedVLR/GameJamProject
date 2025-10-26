34f7efc7626f45133fa89ae42e41f062977ed414
RuinedVLR <rzhurakhov@gmail.com>
2025-10-26 12:34:13 -0300

    Note on scene2

---
+++ Assets/Scripts/NoteScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class NoteScript : MonoBehaviour
+{
+    public void CloseNote(GameObject noteText)
+    {
+        Interactable.isReading = false;
+        noteText.SetActive(false);
+    }
+}
